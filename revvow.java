4c0d55a1ec832b0838b1e85a224deab989ae0848
andriilguvi <andriilguvi68@gmail.com>
2017-07-18 15:52:04 +0530

    Create revvow.java

---
+++ revvow.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class revvow
+{
+public static void main(String args[])
+{
+String s="andriil";
+String rev=new StringBbuffer(s).reverse().toString();
+System.out.println(rev);
+String vowel=rev.replaceAll("[aeiouAEIOU]","");
+System.out.println(vowel);
+}
+}
